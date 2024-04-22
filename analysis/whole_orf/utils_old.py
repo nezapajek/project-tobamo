56fc04e3819160d849bdc8503eda1f577c81d142
nezapajek <neza.arambasic@gmail.com>
2024-04-22 09:30:43 +0200

    script updates

--- analysis/whole_orf/utils_old.py
+++ analysis/whole_orf/utils_old.py
@@ -17,7 +17,7 @@ def compute_identity_score_refxref(args):
     s1, s2 = args
 
     for aln in align_mafft(s1.seq, s2.seq):
-        score, M, N, aln_len, seqA_len, seqB_len, n_aln_len, gaps, gap_ratio, aln_orf_len = score_alignment(aln)
+        score, M, N, aln_len, orf_len, ref_len, n_orflen, gaps = score_alignment(aln)
         break
     return {
         "identity_score": score,
@@ -26,12 +26,10 @@ def compute_identity_score_refxref(args):
         "M": M,
         "N": N,
         "aln_len": aln_len,
-        "ref_len1": seqA_len,
-        "ref_len2": seqB_len,
-        "N/aln_len": n_aln_len,
+        "orf_len": orf_len,
+        "ref_len": ref_len,
+        "N/orf_len": n_orflen,
         "gap_openings": gaps,
-        "gap_ratio": gap_ratio,
-        "aln_orf_len": aln_orf_len,
     }
 
 
@@ -39,7 +37,7 @@ def compute_identity_score_orfxref(args):
     s1, s2 = args
 
     for aln in align_mafft(s1.seq, s2.seq):
-        score, M, N, aln_len, seqA_len, seqB_len, n_aln_len, gaps, gap_ratio, aln_orf_len = score_alignment(aln)
+        score, M, N, aln_len, orf_len, ref_len, n_orflen, gaps = score_alignment(aln)
         break
     return {
         "identity_score": score,
@@ -48,12 +46,10 @@ def compute_identity_score_orfxref(args):
         "M": M,
         "N": N,
         "aln_len": aln_len,
-        "orf_len": seqA_len,
-        "ref_len": seqB_len,
-        "N/aln_len": n_aln_len,
+        "orf_len": orf_len,
+        "ref_len": ref_len,
+        "N/orf_len": n_orflen,
         "gap_openings": gaps,
-        "gap_ratio": gap_ratio,
-        "aln_orf_len": aln_orf_len,
     }
 
 
@@ -72,12 +68,12 @@ def align_mafft(s1, s2):
 def score_alignment(aln):
     M = sum(n1 != n2 for n1, n2 in zip(aln.seqA, aln.seqB) if n1 != "-" and n2 != "-")
     N = sum(1 for n1, n2 in zip(aln.seqA, aln.seqB) if n1 != "-" and n2 != "-")
-    seqA_len = sum(1 for n1 in aln.seqA if n1 != "-")
-    seqB_len = sum(1 for n2 in aln.seqB if n2 != "-")
-    aln_len, aln_orf_len = calculate_alignment_length(aln)
+    orf_len = sum(1 for n1 in aln.seqA if n1 != "-")
+    aln_len = sum(1 for n1, n2 in zip(aln.seqA, aln.seqB))
+    ref_len = sum(1 for n2 in aln.seqB if n2 != "-")
     gaps = gap_openings(aln)
     score = 1 - M / N
-    return score, M, N, aln_len, seqA_len, seqB_len, N / aln_len, gaps, gaps / aln_len, aln_orf_len
+    return score, M, N, aln_len, orf_len, ref_len, N / orf_len, gaps
 
 
 def gap_openings(aln):
@@ -93,19 +89,7 @@ def gap_openings(aln):
     return gaps
 
 
-def calculate_alignment_length(aln):
-    match_positions = [i for i, (n1, n2) in enumerate(zip(aln.seqA, aln.seqB)) if n1 != "-" and n2 != "-"]
-    if match_positions:
-        first_match = match_positions[0]
-        last_match = match_positions[-1] + 1
-        aln_len = last_match - first_match
-    else:
-        aln_len = 0
-    aln_orf_len = aln_len - sum(1 for n1 in aln.seqA[match_positions[0] : match_positions[-1] + 1] if n1 == "-")
-    return aln_len, aln_orf_len
-
-
-def df_ref_edit(results):
+def df_edit(results):
     df = pd.DataFrame(results)
     df = df[
         [
@@ -113,14 +97,12 @@ def df_ref_edit(results):
             "ref_name2",
             "identity_score",
             "gap_openings",
-            "gap_ratio",
-            "N/aln_len",
-            "aln_orf_len",
+            "N/orf_len",
             "M",
             "N",
             "aln_len",
-            "ref_len1",
-            "ref_len2",
+            "orf_len",
+            "ref_len",
         ]
     ]
     return df
@@ -138,9 +120,7 @@ def df_orf_edit(results):
             "ref_name",
             "identity_score",
             "gap_openings",
-            "gap_ratio",
-            "N/aln_len",
-            "aln_orf_len",
+            "N/orf_len",
             "M",
             "N",
             "aln_len",
@@ -153,28 +133,3 @@ def df_orf_edit(results):
 
 def remove_last_number(s):
     return re.sub(r"_(\d+)$", "", s)
-
-
-def compute_identity_score_print_aln(args):
-    s1, s2 = args
-    retl = []
-
-    aln = align_mafft(s1.seq, s2.seq)[0]
-    a = "".join([" ", "|"][n1 == n2] for n1, n2 in zip(aln.seqA, aln.seqB))
-    aln_str = "\n".join([aln.seqA, a, aln.seqB])
-    score, M, N, aln_len, _, _, n_aln_len, gaps, _ = score_alignment(aln)
-    retl = [
-        "**********************************************************************************\n",
-        s1.id + "\n",
-        s2.id + "\n",
-        f"score: {score}" + "\n",
-        f"M: {M}" + "\n",
-        f"N: {N}" + "\n",
-        f"aln_len: {aln_len}" + "\n",
-        f"N/aln_len: {n_aln_len}" + "\n",
-        f"gap_openings: {gaps}",
-        "\n\n",
-        aln_str,
-        "\n\n",
-    ]
-    return retl
